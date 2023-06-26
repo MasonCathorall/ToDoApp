c3c49acb87a603428be371fc2957aaedebd54d6c
MasonCathorall <masoncathorallrev@gmail.com>
2023-06-26 18:01:02 -0400

    Added all CRUD methods to API

--- ToDoAPI/Controllers/ToDoController.cs
+++ ToDoAPI/Controllers/ToDoController.cs
@@ -21,5 +21,59 @@ namespace ToDoAPI.Controllers
         {
             return await _context.Todos.ToListAsync();
         }
+
+        //Get todo by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDo(Guid id)
+        {
+            return await _context.Todos.Where(x => x.Id == id).ToListAsync();
+        }
+
+        //post flashcard
+        [HttpPost]
+        public async Task<ActionResult<ToDo>> PostToDo(ToDo c)
+        {
+            c.Id = Guid.Empty;
+            _context.Todos.Add(c);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostToDo", new { id = c.Id }, c);
+        }
+
+        //delete flashcard
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ToDo>> DeleteToDo(Guid id)
+        {
+            var card = await _context.Todos.FindAsync(id);
+
+
+            if (card is null)
+                return NotFound();
+
+            _context.Todos.Remove(card);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //update flashcard
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateToDo(Guid id, ToDo card)
+        {
+            _context.Entry(card).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Todos.Any(e => e.Id == id))
+                    return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
     }
 }
